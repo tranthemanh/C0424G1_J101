87057aaf28a41e62ae6157779b72ce4037dd8323
tranthemanh <tranmanhhuha@gmail.com>
2024-06-11 17:33:51 +0700

    update file QLSV

--- Module2/Bai4_OOP/QLSV/src/QLSV.java
+++ Module2/Bai4_OOP/QLSV/src/QLSV.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class QLSV {
@@ -13,6 +12,13 @@ public class QLSV {
         this.size = 0;
     }
 
+    /*
+     * Ý Tưởng:
+     * Lấy ra vị trí của sinh vien có mã SV tương ứng với mã SV
+     * Nếu có vị trí tương ứng thì sẽ thông báo là đã có mã sinh viên
+     * Nếu chưa có vị trí tương ứng thì khai bao 1 sinh viên mới với mã sinh vien, tên, email, sdt, pga mới
+     * Them sinh vien mới tạo vào danh sách
+     */
     public void themSinhVien(){
         if(this.size == this.limit){
             System.out.println("Danh sach sinh vien da day.");
@@ -20,8 +26,6 @@ public class QLSV {
             System.out.println("Nhap ma sinh vien: ");
             int maSV = scanner.nextInt();
             scanner.nextLine();
-
-            //Kiem tra ma sinh vien co ton tai chua
             int vt = -1;
             for (int i = 0; i < this.size; i++) {
                 if (danhSachSinhVien[i].getMaSV() == maSV){
@@ -61,6 +65,12 @@ public class QLSV {
         }
     }
 
+    /*
+     * Ý Tưởng:
+     * Lấy ra vị trí của sinh vien có mã SV tương ứng với mã SV
+     * chuyển sinh viên tương ứng xuống cuối mảng
+     * Cho sinh vien đó bằng null
+     * */
     public void xoaSinhVien(){
         int vt = -1;
         System.out.println("Nhap ma sinh vien can xoa: ");
@@ -73,15 +83,34 @@ public class QLSV {
         if(vt == -1){
             System.out.println("Khong co sinh vien nao phu hop.");
         }else {
-            for (int j = vt; j < this.size - 1; j++) {
-                danhSachSinhVien[j] = danhSachSinhVien[j+1];
-            }
-            danhSachSinhVien[this.size-1] = null;
-            this.size--;
-            System.out.println("Xoa sinh vien thanh cong. ");
+            int chon;
+            do {
+                System.out.println("Ban chac chan muon xoa khong!");
+                System.out.println("Chac chan nhan 1");
+                System.out.println("Khong muon xoa nhan 2");
+                System.out.println("Nhap lua chon: ");
+                chon = scanner.nextInt();
+                switch (chon){
+                    case 1:
+                        for (int j = vt; j < this.size - 1; j++) {
+                            danhSachSinhVien[j] = danhSachSinhVien[j+1];
+                        }
+                        danhSachSinhVien[this.size-1] = null;
+                        this.size--;
+                        System.out.println("Xoa sinh vien thanh cong. ");
+                        break;
+                }
+            }while (chon != 2);
+
         }
     }
 
+    /*
+     * Ý Tưởng:
+     * Lấy ra vị trí của sinh vien có mã SV tương ứng với mã SV
+     * khai bao 1 sinh viên mới với tên, email, sdt, pga mới
+     * Cho sinh vien tương ứng với vị trí bằng sinh viên mới tạo
+     * */
     public void suaSinhVien(){
         int vt = -1;
         System.out.println("Nhap ma sinh vien can sua: ");
