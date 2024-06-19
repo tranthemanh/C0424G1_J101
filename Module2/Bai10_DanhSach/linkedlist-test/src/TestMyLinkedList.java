26ea14ac7e573e471ec9f7ce8fb06cd5fc7eaeb7
tranthemanh <tranmanhhuha@gmail.com>
2024-06-19 14:37:56 +0700

    add file linkedlist-test

---
+++ Module2/Bai10_DanhSach/linkedlist-test/src/TestMyLinkedList.java
@@ -0,0 +1,12 @@
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList(10);
+        myLinkedList.addFirst(11);
+        myLinkedList.addFirst(12);
+        myLinkedList.addFirst(13);
+
+        myLinkedList.add(4,9);
+        myLinkedList.add(4,9);
+        myLinkedList.printList();
+    }
+}
