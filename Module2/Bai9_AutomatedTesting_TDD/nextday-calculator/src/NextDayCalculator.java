207213568dffb5df515d2954e9e0d52beb510c72
tranthemanh <tranmanhhuha@gmail.com>
2024-06-19 10:21:27 +0700

    update file nextday-calculator

--- Module2/Bai9_AutomatedTesting_TDD/nextday-calculator/src/NextDayCalculator.java
+++ Module2/Bai9_AutomatedTesting_TDD/nextday-calculator/src/NextDayCalculator.java
@@ -4,10 +4,10 @@ public class NextDayCalculator {
         int nextMonth = month;
         int nextYear = year;
 
-        if(day < 1 || day > 31 || month < 1 || month > 12) {
+        if(CheckForValidDateAndMonth(day, month)) {
             System.out.println("Khong ton tai");
         }
-        if (day == 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)) {
+        if (CheckTheMonthHas31Days(day, month)) {
             nextDay = 1;
             if (month == 12) {
                 nextMonth = 1;
@@ -15,16 +15,16 @@ public class NextDayCalculator {
             }else {
                 nextMonth = month + 1;
             }
-        }else if (day == 30 && (month == 4 || month == 6 || month == 9 || month == 11)) {
+        }else if (CheckTheMonthHas30Days(day, month)) {
             nextDay = 1;
             nextMonth = month + 1;
         }else if(month == 2) {
-            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+            if (checkLeapYear(year)){
                 if (day == 29) {
                     nextDay = 1;
                     nextMonth = month + 1;
                 }
-            } else if (day == 28 && month == 2){
+            } else if (day == 28){
                 nextDay = 1;
                 nextMonth = month + 1;
             }
@@ -32,5 +32,21 @@ public class NextDayCalculator {
             nextDay++;
         }
         return new int[] {nextDay, nextMonth, nextYear};
+    }
+
+    private static boolean checkLeapYear(int year) {
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+    }
+
+    private static boolean CheckTheMonthHas30Days(int day, int month) {
+        return day == 30 && (month == 4 || month == 6 || month == 9 || month == 11);
+    }
+
+    private static boolean CheckTheMonthHas31Days(int day, int month) {
+        return day == 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12);
+    }
+
+    private static boolean CheckForValidDateAndMonth(int day, int month) {
+        return day < 1 || day > 31 || month < 1 || month > 12;
     }
 }
