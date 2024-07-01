655a03156798e626bd48cd36f9a1ac58fff7ff30
tranthemanh <tranmanhhuha@gmail.com>
2024-07-01 15:08:39 +0700

    add file test_thread_1

---
+++ Module2/Bai18_Threading/test_thread_1/src/Count.java
@@ -0,0 +1,25 @@
+public class Count implements Runnable {
+    private Thread myThread;
+
+    public Count() {
+        myThread = new Thread(this, "My runnable thread");
+        System.out.println("My thread created: " + myThread);
+        myThread.start();
+    }
+
+    public Thread getMyThread() {
+        return myThread;
+    }
+
+    public void run() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Printing the counter: " + i);
+                Thread.sleep(1000);
+            }
+        }catch (InterruptedException e){
+            System.out.println("Thread interrupted");
+        }
+        System.out.println("My thread run is over.");
+    }
+}
