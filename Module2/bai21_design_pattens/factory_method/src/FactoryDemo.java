c22f949f4d758e834564e080f6b538573f08859a
tranthemanh <tranmanhhuha@gmail.com>
2024-07-04 10:34:42 +0700

    add file facetory_method

---
+++ Module2/bai21_design_pattens/factory_method/src/FactoryDemo.java
@@ -0,0 +1,11 @@
+public class FactoryDemo {
+    public static void main(String[] args) {
+        FactoryAnimal factoryAnimal = new FactoryAnimal();
+
+        Animal a1 = factoryAnimal.getAnimal("feline");
+        System.out.println("a1 sound: " + a1.makeSound());
+
+        Animal a2 = factoryAnimal.getAnimal("canine");
+        System.out.println("a2 sound: " + a2.makeSound());
+    }
+}
